a6e0b0ac6c6cee68e0436d6bc7c23e2837f97ba2
CourtneyHoppus <74206103+CourtneyHoppus@users.noreply.github.com>
2021-04-01 13:46:27 -0700

    fixed order of app.use

--- service/index.js
+++ service/index.js
@@ -7,11 +7,11 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = `mongodb+srv://courtney:TOS7fUKvdGSin4sA@testcluster.2t0xa.mongodb.net/testDatabase?retryWrites=true&w=majority`;
 const PORT = process.env.PORT || 5000;

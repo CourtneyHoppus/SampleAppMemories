120c67ea36175e93acdb1454ecd8c7d88b7d96e2
CourtneyHoppus <74206103+CourtneyHoppus@users.noreply.github.com>
2021-04-01 15:27:38 -0700

    added to client

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import { useDispatch } from 'react-redux';
 import { getPosts } from './actions/posts';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
-import memories from './images/test4.png';
+import memories from './images/memories.png';
 import useStyles from './styles';
 
 const App = () => {
